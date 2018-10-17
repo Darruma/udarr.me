59b76b8aae44abdf1efe1b0ce63574b838b8644b
Darruma <umerderr@gmail.com>
2018-10-17 22:05:45 +0100

    added heroku-postbuild

--- frontend/src/components/main.jsx
+++ frontend/src/components/main.jsx
@@ -11,10 +11,12 @@ class Main extends Component
                 <div className="title">
                      Umair Darr
                 </div>
+                <div className='link-container'>
                 <Link to="/projects" className='links'>Projects</Link>
                 <Link to="/photography" className='links'>Photography</Link>
                 <Link to="/blog" className='links'>Blog</Link>
                 <a className='links' href="https://github.com/Darruma">Github</a>
+                </div>
             </div>
         </div>)
     }
