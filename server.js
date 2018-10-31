585b2ceb01cc518e6ecfe40de399169ec5b4db35
Darruma <umerderr@gmail.com>
2018-10-31 22:21:57 +0000

    cert

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const app = require('./app');
 const compression = require('compression')
+
+app.get('/.well-known/acme-challenge/:content', function(req, res) {
+  res.send('PMi611XBM1208bVUMrtXD5WQri9Ybb9ZuR8wz7C3MTU.fCnANhc3gdW_Kz6FppVJL4_fhVuqT9VsbkBwCpPttpk');
+})
 app.use(compression());
 app.listen(process.env.PORT,function()
 {
