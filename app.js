bd7377c3b078b7f807cc681c4ae124a741a76bf0
Darruma <umerderr@gmail.com>
2018-10-31 22:26:50 +0000

    cert added

--- app.js
+++ app.js
@@ -5,23 +5,23 @@ const bodyParser = require('body-parser');
 const app = express();
 const session = require('express-session');
 app.enable('trust proxy');
-app.use(function(req, res, next) {
-	if (req.secure) {
-		// request was via https, so do no special handling
-		next();
-	} else {
-		// request was via http, so redirect to https
-		res.redirect('https://' + req.headers.host + req.url);
-	}
-});
+// app.use(function(req, res, next) {
+// 	if (req.secure) {
+// 		// request was via https, so do no special handling
+// 		next();
+// 	} else {
+// 		// request was via http, so redirect to https
+// 		res.redirect('https://' + req.headers.host + req.url);
+// 	}
+// });
 app.set('json spaces', 2);
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(session({ secret: 'yeet', resave: false, saveUninitialized: true }));
 
-app.get('/.well-known/acme-challenge/h5QbsV45_uStRE3bnknBqonBSetlIbjXGY2IoA1anJ4',(req,res) =>
+app.get('/.well-known/acme-challenge/DSOORnfKe8RWJaQ0Czoejn9Ejeto8XCLcR_Ijh54GwM',(req,res) =>
 {
-	res.send(h5QbsV45_uStRE3bnknBqonBSetlIbjXGY2IoA1anJ4.fCnANhc3gdW_Kz6FppVJL4_fhVuqT9VsbkBwCpPttpk);
+	res.send('DSOORnfKe8RWJaQ0Czoejn9Ejeto8XCLcR_Ijh54GwM.fCnANhc3gdW_Kz6FppVJL4_fhVuqT9VsbkBwCpPttpk');
 })
 app.use(express.static(path.join(__dirname,'frontend','build')));
 app.use('/images', express.static(path.join(__dirname, '/uploads')));
