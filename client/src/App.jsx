062013c22fdd02e30fe470f6ecb953a254bb23d6
Darruma <umerderr@gmail.com>
2018-11-27 12:32:58 +0000

    added editor redirect

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,8 +5,8 @@ import Blog from './components/Blog'
 import Main from './components/Main'
 import BlogPost from './components/BlogPost'
 import { BrowserRouter as Router, Route,Switch} from "react-router-dom";
+import BlogEditor from './components/BlogEditor';
 class App extends Component {
-
 	render() {
 		return (
 			<Router >
@@ -16,6 +16,7 @@ class App extends Component {
 					<Route path="/projects" component={Projects}/>
 					<Route exact path="/blog" component={Blog}/>
 					<Route path='/blog/:id' component={BlogPost}/>
+					<Route path= '/blogeditor' component={BlogEditor}/>
 					<Route path="/photography" component={Projects}/>
 				</Switch>
 			</Router>
--- client/src/components/BlogEditor.jsx
+++ client/src/components/BlogEditor.jsx
@@ -1,15 +1,21 @@
 import React , { Component } from 'react'
 import '../css/editor.css'
+import { Redirect } from 'react-router-dom'
 class BlogEditor extends Component
 {
     state ={
         blog:{
             title:'',
             content:[]
-        }
+        },
+        loggedIn:true
     }
     render()
     {
+        if (!this.state.loggedIn)
+        {
+            return (<Redirect to='/'></Redirect>)
+        }
         <div className='editor-container'>
         <input onChange={this.handleTitleChange}></input>
             <div className='editor-typing'>
@@ -27,6 +33,12 @@ class BlogEditor extends Component
         var blog_changed = this.state.blog;
         blog_changed.title = e.target.value
         this.setState({blog:blog_changed})
+    }
+    componentDidMount = () =>
+    {
+        fetch('/api/authenticate')
+        .then(res => res.json())
+        .then(res => this.setState({loggedIn:res.success}))
     }
 }
 export default BlogEditor
