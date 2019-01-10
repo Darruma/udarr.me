ab4bd345f77f67e316140d1860adb2d4f58ed4bc
unknown <umerderr@gmail.com>
2019-01-10 21:25:54 +0000

    added images

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const app = require('./app');
 const compression = require('compression')
 const port = process.env.PORT || 3005
-console.log(port)
 app.use(compression());
 app.listen(port,function()
 {
