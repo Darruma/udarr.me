80eadf77b988f888c4179c5836752bf049aae328
unknown <umerderr@gmail.com>
2019-02-02 21:13:11 +0000

    added update function to change project data periodically

--- routes/projects.js
+++ routes/projects.js
@@ -2,32 +2,20 @@ const express = require('express');
 const fetch = require('node-fetch');
 var projectData = []
 const router = express.Router();
-setInterval(getProjects, 60000)
+getProjects()
+setInterval(updateProjects, 60000)
+
 router.get('/projects', (req, res) => {
 	res.send(projectData);
 });
 
-
 function getProjects() {
-	fetch('https://api.github.com/users/Darruma/repos?access_token=' + process.env.PERSONAL_ACCESS_TOKEN).then(res => res.json()).then(res => {
-		projectData = []
+
+	fetchRepos().then(res => {
 		res.forEach(element => {
-			var { name } = element
-			var { pushed_at } = element
-			var { description } = element
-			var technologies = [];
 			fetch(element.languages_url + '?access_token=' + process.env.PERSONAL_ACCESS_TOKEN).then(res => res.json()).then(res => {
 				technologies = Object.keys(res).map(e => e.toLowerCase())
-				projectData.push(
-					{
-						title: name,
-						content: description,
-						link: element.html_url,
-						technologies: technologies,
-						id: pushed_at,
-						webpage: element.homepage
-					}
-				)
+				addProject(element.name, element.description, element.html_url, technologies, element.pushed_at, element.homepage)
 				projectData.sort((a, b) => {
 					return new Date(b.id) - new Date(a.id)
 				})
@@ -36,4 +24,48 @@ function getProjects() {
 	}
 	)
 }
+
+function updateProjects() {
+	fetchRepos().then(res => {
+		projectData = projectData.filter(project => {
+			if (res.map(e => e.name).includes(project.name)) {
+				return true
+			}
+			return false
+		})
+		res.forEach(element => {
+			fetch(element.languages_url + '?access_token=' + process.env.PERSONAL_ACCESS_TOKEN).then(res => res.json()).then(res => {
+				technologies = Object.keys(res).map(e => e.toLowerCase())
+				projectData.forEach(e => {
+					if (e.name !== element.name) {
+						addProject(element.name, element.description, element.html_url, technologies, element.pushed_at, element.homepage)
+					}
+				})
+
+				projectData.sort((a, b) => {
+					return new Date(b.id) - new Date(a.id)
+				})
+			})
+		})
+	})
+}
+function fetchRepos() {
+	return fetch('https://api.github.com/users/Darruma/repos?access_token=' + process.env.PERSONAL_ACCESS_TOKEN).then(res => res.json())
+}
+
+
+function addProject(name, description, html_url, technologies, pushed_at, homepage) {
+	projectData.push(
+		{
+			title: name,
+			content: description,
+			link: html_url,
+			technologies: technologies,
+			id: pushed_at,
+			webpage: element.homepage
+		}
+	)
+}
+
+
 module.exports = router;
