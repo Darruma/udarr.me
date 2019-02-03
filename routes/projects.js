f76cb14664944d6b81f011c4d7ec109568fe5d58
unknown <umerderr@gmail.com>
2019-02-03 11:48:11 +0000

    simplifed updating method

--- routes/projects.js
+++ routes/projects.js
@@ -12,6 +12,18 @@ router.get('/projects', (req, res) => {
 function getProjects() {
 
 	fetchRepos().then(res => {
+		var names = res.map(e => e.name)
+		if (projectData.length !== 0) {
+
+			projectData = projectData.filter(project => {
+
+				if (names.includes(project.title)) {
+					return true
+				}
+				console.log('filtered repository')
+				return false
+			})
+		}
 		res.forEach(element => {
 			fetch(element.languages_url + '?access_token=' + process.env.PERSONAL_ACCESS_TOKEN).then(res => res.json()).then(res => {
 				technologies = Object.keys(res).map(e => e.toLowerCase())
@@ -26,49 +38,39 @@ function getProjects() {
 	)
 }
 
-function updateProjects() {
-	console.log('updating projects')
-	fetchRepos().then(res => {
-		var names = res.map(e => e.name)
-		var projectDataNames = projectData.map(e => e.title)
-		projectData = projectData.filter(project => {
 
-			if (names.includes(project.title)) {
-				return true
-			}
-			console.log('filtered repository')
-			return false
-		})
-		res.forEach(element => {
-			fetch(element.languages_url + '?access_token=' + process.env.PERSONAL_ACCESS_TOKEN).then(res => res.json()).then(res => {
-				technologies = Object.keys(res).map(e => e.toLowerCase())
-				if (!projectDataNames.includes(element.name)) {
-					addProject(element.name, element.description, element.html_url, technologies, element.pushed_at, element.homepage)
-				}
-
-				projectData.sort((a, b) => {
-					return new Date(b.id) - new Date(a.id)
-				})
-			})
-		})
-	})
-}
 function fetchRepos() {
 	return fetch('https://api.github.com/users/Darruma/repos?access_token=' + process.env.PERSONAL_ACCESS_TOKEN).then(res => res.json())
 }
 
-
 function addProject(name, description, html_url, technologies, pushed_at, homepage) {
-	projectData.push(
-		{
-			title: name,
-			content: description,
-			link: html_url,
-			technologies: technologies,
-			id: pushed_at,
-			webpage: homepage
-		}
-	)
+	// Check if the project is already in project data
+	var projectDataNames = projectData.map(e => e.title)
+	if (projectDataNames.includes(name)) {
+		var indexOfProject = projectDataName.indexOf(name)
+		projectData[indexOfProject] =
+			{
+				title: name,
+				content: description,
+				link: html_url,
+				technologies: technologies,
+				id: pushed_at,
+				webpage: homepage
+			}
+
+	}
+	else {
+		projectData.push(
+			{
+				title: name,
+				content: description,
+				link: html_url,
+				technologies: technologies,
+				id: pushed_at,
+				webpage: homepage
+			}
+		)
+	}
 }
 
 
