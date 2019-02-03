eabfa2f429c61bc98c35b0118390909f6d4ece32
unknown <umerderr@gmail.com>
2019-02-03 11:01:30 +0000

    changed project updating

--- routes/projects.js
+++ routes/projects.js
@@ -30,28 +30,28 @@ function updateProjects() {
 	console.log('updating projects')
 	fetchRepos().then(res => {
 		var names = res.map(e => e.name)
-		projectData = projectData.filter(project => {
+		// projectData = projectData.filter(project => {
 			
-			if (names.includes(project.title)) {
-				return true
-			}
-			return false
-		})
-		console.log('Filtered Projects')
-		res.forEach(element => {
-			fetch(element.languages_url + '?access_token=' + process.env.PERSONAL_ACCESS_TOKEN).then(res => res.json()).then(res => {
-				technologies = Object.keys(res).map(e => e.toLowerCase())
-				projectData.forEach(e => {
-					if (e.name !== element.name) {
-						addProject(element.name, element.description, element.html_url, technologies, element.pushed_at, element.homepage)
-					}
-				})
+		// 	if (names.includes(project.title)) {
+		// 		return true
+		// 	}
+		// 	return false
+		// })
+		// console.log('Filtered Projects')
+		// res.forEach(element => {
+		// 	fetch(element.languages_url + '?access_token=' + process.env.PERSONAL_ACCESS_TOKEN).then(res => res.json()).then(res => {
+		// 		technologies = Object.keys(res).map(e => e.toLowerCase())
+		// 		projectData.forEach(e => {
+		// 			if (e.name !== element.name) {
+		// 				addProject(element.name, element.description, element.html_url, technologies, element.pushed_at, element.homepage)
+		// 			}
+		// 		})
 
-				projectData.sort((a, b) => {
-					return new Date(b.id) - new Date(a.id)
-				})
-			})
-		})
+		// 		projectData.sort((a, b) => {
+		// 			return new Date(b.id) - new Date(a.id)
+		// 		})
+		// 	})
+		// })
 	})
 }
 function fetchRepos() {
