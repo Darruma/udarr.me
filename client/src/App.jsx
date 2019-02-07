b1861f211430affc61318dec27bf6e0ee388e6ab
unknown <umerderr@gmail.com>
2019-02-07 14:19:06 +0000

    changed min width to 90% to match default

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import Projects from './components/projects';
 import Article from './components/Article'
 import Main from './components/main'
 import { BrowserRouter as Router, Route,Switch} from "react-router-dom";
