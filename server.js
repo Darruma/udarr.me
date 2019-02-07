81c333c9e2068f65c920961e84bb20a9db5ebc3f
unknown <umerderr@gmail.com>
2019-02-07 12:06:38 +0000

    changed server file

--- server.js
+++ server.js
@@ -2,9 +2,7 @@ const app = require('./app');
 const compression = require('compression')
 const port = process.env.PORT || 3005
 app.use(compression());
-// app.listen(port,function()
-// {
-// 	console.log("Website running " + port);
-// });
-var server = require('http').createServer(app)
-server.listen(3005)
+app.listen(port,function()
+{
+	console.log("Website running " + port);
+});
