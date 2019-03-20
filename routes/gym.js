675316ad6d3f7820c5a016321f46af3e6a74e5a4
unknown <umerderr@gmail.com>
2019-03-20 19:00:54 +0000

    added gym script

--- routes/gym.js
+++ routes/gym.js
@@ -1,12 +1,28 @@
 const express = require('express'); 
-const fetch = require('node-fetch');
+const exec = require('child_process').exec;
 var sys = require('sys');
 const router = express.Router(); 
 require('dotenv').config()
 router.get('/gymamount',(req,res) =>
 {
-
+    const gym_script = exec("sh gym.sh")
+    gym_script.stdout.on('data', function(data){
+        res.send(
+            {
+                success:true,
+                amount:data
+            }
+        )
+        
+    });
+    gym_script.stderr.on('data', function(data){
+        res.send(
+            {
+                success:false,
+                amount:-1
+            }
+        )
+    });
 })
-
 
 module.exports = router;
