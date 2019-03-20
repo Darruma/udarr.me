10b90172d8efaf981799d6a89c23f39f019bd0a5
unknown <umerderr@gmail.com>
2019-03-20 12:52:19 +0000

    added gym route

---
+++ routes/gym.js
@@ -0,0 +1,12 @@
+const express = require('express'); 
+const fetch = require('node-fetch');
+var sys = require('sys');
+const router = express.Router(); 
+require('dotenv').config()
+router.get('/gymamount',(req,res) =>
+{
+
+})
+
+
+module.exports = router;
