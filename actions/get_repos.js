f11dca3e26659ee869124aefdb73ca5d23dd7e95
darruma <umerderr@gmail.com>
2019-04-23 13:51:12 +0100

    converted filesystem to async await

--- actions/get_repos.js
+++ actions/get_repos.js
@@ -1,9 +1,15 @@
 const fetch_authenticated = require('../actions/fetch_authenticated');
-module.exports = () => {
-    const fetch_repos = fetch_authenticated('https://api.github.com/users/Darruma/repos').then(res => res.json());
-    return fetch_repos.then(repos => {
-        let readme_proms = repos.map(element => fetch_authenticated('https://api.github.com/repos/Darruma/' + element.name + '/readme').then(res => res.json()))
-        let proms = [...repos.map(element => fetch_authenticated(element.languages_url).then(res => res.json())), fetch_repos];
-        return Promise.all(readme_proms.concat(proms));
-    });
+module.exports = async () => {
+    try {
+        const repos = await fetch_authenticated('https://api.github.com/users/Darruma/repos')
+            .then(res => res.json());
+        const repos_info = await Promise.all([...repos.map(repo => {
+            return fetch('https://api.github.com/repos/Darruma/' + element.name + '/readme')
+                .then(res => res.json())
+        }), repos.map(repo => repo.languages_url).then(res => res.json)])
+        return [...repos_info, repos]
+    } catch (err) {
+        return []
+    }
+
 }
--- routes/filesystem.js
+++ routes/filesystem.js
@@ -1,13 +1,26 @@
 const express = require('express');
 const router = express.Router();
 const get_repos = require('../actions/get_repos');
-
-function create_directory(name) {
-    return {
-        name: name,
-        type: 'directory',
-        children: []
-    }
+let fs = {
+    name: '/',
+    type: 'directory',
+    children: [
+        {
+            name: 'projects',
+            type: 'directory',
+            children: []
+        },
+        {
+            name: 'blog',
+            type: 'directory',
+            children: []
+        },
+        {
+            name: 'instructions.txt',
+            type: 'file',
+            data: 'Hello'
+        },
+    ]
 }
 function base64_to_ascii(b, name) {
     if (b == undefined) {
@@ -16,45 +29,27 @@ function base64_to_ascii(b, name) {
         return Buffer.from(b, 'base64').toString('ascii');
     }
 }
-
-router.get('/filesystem', (req, res) => {
-    let fs = {
-        name: '/',
-        type: 'directory',
-        children: [
-            {
-                name: 'projects',
-                type: 'directory',
-                children: []
-            },
-            {
-                name: 'blog',
-                type: 'directory',
-                children: []
-            },
-            {
-                name: 'instructions.txt',
-                type: 'file',
-                data: 'Hello'
-            },
-        ]
-    }
-    get_repos().then(values => {
-        const repo_data = values[values.length - 1]
+router.get('/filesystem', async (req, res) => {
+    try {
+        const data = await get_repos()
+        const repo_data = data[data.length - 1]
         const repos_amount = repo_data.length
-        const language_objects = values.slice(repos_amount, values.length - 1)
-        let langs = new Set();
-        language_objects.forEach(obj => {
-            Object.keys(obj).forEach(lang => langs.add(lang))
-        })
-        langs.forEach(l => {
-            fs.children[0].children.push(create_directory(l));
+        const language_objects = data.slice(repos_amount, data.length - 1)
+        const langs = Array.from(new Set(language_objects.reduce((arr, obj) => {
+            return arr.concat(Object.keys(obj))
+        })))
+        fs.children[0].children = langs.map(l => {
+            return {
+                name: l,
+                type: 'directory',
+                children: []
+            }
         })
         repo_data.forEach((repo, index) => {
-            const readme = base64_to_ascii(values[index].content, repo.name);
+            const readme = base64_to_ascii(data[index].content, repo.name);
             repo_languages = Object.keys(language_objects[index])
             repo_languages.forEach(lang => {
-                let folderIndex = fs.children[0].children.findIndex(language_folder => language_folder.name == lang);
+                const folderIndex = fs.children[0].children.findIndex(language_folder => language_folder.name == lang);
                 fs.children[0].children[folderIndex].children.push(
                     {
                         name: repo.name,
@@ -64,9 +59,7 @@ router.get('/filesystem', (req, res) => {
                             type: 'file',
                             data: readme
                         }]
-                    }
-
-                )
+                    })
             })
         })
         res.send({
@@ -74,7 +67,12 @@ router.get('/filesystem', (req, res) => {
             filesystem: fs,
         })
     }
-    )
+    catch (err) {
+        res.send({
+            success: false,
+            error: 'Error'
+        })
+    }
 })
 
 module.exports = router;
