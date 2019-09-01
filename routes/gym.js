532cd6d2c2fe86e93066304b39b83574b248cf77
unknown <umair.darr@levelprime.com>
2019-09-01 13:35:13 +0100

    Used kvdb to store gymamount

--- routes/gym.js
+++ routes/gym.js
@@ -1,25 +1,15 @@
 const express = require('express');
-const exec = require('child_process').exec;
 const router = express.Router();
+const fetch = require('node-fetch')
 require('dotenv').config()
 router.get('/gymamount', (req, res) => {
-    const gym_script = exec("sh gym.sh")
-    gym_script.stdout.on('data', function (data) {
-        res.send(
-            {
-                success: true,
-                amount: data
-            }
-        )
-
-    });
-    gym_script.stderr.on('data', function (data) {
-        res.send(
-            {
-                success: false,
-                amount: -1
-            }
-        )
-    });
+   fetch(`https://kvdb.io/${process.env.KVDB_ID}/gym`)
+   .then(response => response.json()).then(amount => {
+       res.send({
+           success:true,
+           amount
+       })
+   }
+   )
 })
 module.exports = router;
