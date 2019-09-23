db18768913715ed1234396a1d73f1470ef4db173
unknown <umair.darr@levelprime.com>
2019-09-23 17:38:43 +0100

    added way to save gym data every 7.5 minutes

--- routes/gym.js
+++ routes/gym.js
@@ -1,15 +1,32 @@
 const express = require('express');
 const router = express.Router();
+const fs = require('fs');
+const path = require('path');
+
 const fetch = require('node-fetch')
+function captureData() {
+    let data = JSON.parse(fs.readFileSync(path.join(__dirname, "gym_data.json")));
+    fetch(`https://kvdb.io/${process.env.KVDB_ID}/gym`)
+        .then(response => response.json()).then(res => {
+            const dateTime = new Date().toString();
+            let gymElement = {}
+            gymElement[dateTime] = res
+            data.push(gymElement)
+            console.log(data)
+            fs.writeFileSync(path.join(__dirname, "gym_data.json"), JSON.stringify(data, null, 2));
+        })
+}
+setInterval(captureData, 1000 * 10 * 60);
+
 require('dotenv').config()
 router.get('/gymamount', (req, res) => {
-   fetch(`https://kvdb.io/${process.env.KVDB_ID}/gym`)
-   .then(response => response.json()).then(amount => {
-       res.send({
-           success:true,
-           amount
-       })
-   }
-   )
+    fetch(`https://kvdb.io/${process.env.KVDB_ID}/gym`)
+        .then(response => response.json()).then(amount => {
+            res.send({
+                success: true,
+                amount
+            })
+        }
+        )
 })
 module.exports = router;
