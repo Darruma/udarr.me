2ed34ccc53948406b6cc1f834c480020202cc554
unknown <umair.darr@levelprime.com>
2019-10-07 22:06:55 +0100

    added parser for activity data

--- utils/scrape_gym.js
+++
@@ -1,8 +0,0 @@
-const fetch = require('node-fetch')
-const scrape_options = require('../scrape_options')
-fetch("https://www.puregym.com/api/members/login/",scrape_options)
-
-fetch('https://www.puregym.com/members/').then(resp => resp.text()).then(res => {
-    console.log(res)
-}
-)
