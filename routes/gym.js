7f12823149678b4bcc9df485ab44fcea6830f4d7
Darruma <darruma@protonmail.com>
2019-10-08 14:32:18 +0100

    added activity endpoint for year info

--- routes/gym.js
+++ routes/gym.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const get_activity = require('../utils/get_activity')
 const fs = require('fs');
 const path = require('path');
-const minutes = 7.5
 const fetch = require('node-fetch')
+const minutes = 7.5
+require('dotenv').config();
 function captureData() {
     let data = JSON.parse(fs.readFileSync(path.join(__dirname, "gym_data.json")));
     fetch(`https://kvdb.io/${process.env.KVDB_ID}/gym`)
@@ -12,10 +14,16 @@ function captureData() {
             let gymElement = {}
             gymElement[dateTime] = res
             data.push(gymElement)
-            console.log(data)
             fs.writeFileSync(path.join(__dirname, "gym_data.json"), JSON.stringify(data, null, 2));
         })
 }
+
+function captureActivity() {
+    fs.writeFileSync(path.join(__dirname,"activity.json"),JSON.stringify(get_activity(),null,2))
+}
+captureActivity()
+captureData()
+setInterval(captureActivity,1000 * 60 * 60)
 setInterval(captureData, 1000 * 60 * minutes);
 require('dotenv').config()
 router.get('/gymamount', (req, res) => {
@@ -37,5 +45,19 @@ router.get('/gym', (req, res) => {
             success: true,
             data: data
         })
+})
+
+router.get('/activity',(req,res) => {
+   let data = JSON.parse(fs.readFileSync(path.join(__dirname,"activity.json")));
+   if(data !=undefined) {
+       res.send({
+           success:true,
+           data:data
+       })
+   } else {
+       res.send({
+           success:false
+       })
+   }
 })
 module.exports = router;
--- utils/get_activity.js
+++ utils/get_activity.js
@@ -1,34 +1,38 @@
 const htmlparser = require('node-html-parser').parse
 const fs = require('fs');
 const path = require('path')
-const activity_html_string = fs.readFileSync(path.join(__dirname, "..", "routes", "activity.html"), "utf8")
-const root = htmlparser(activity_html_string)
-const activity_html_array = root.querySelector(".calendar-column").childNodes.slice(4).filter(element => {
-    return element.nodeType == 1
-})
-const activity = activity_html_array.map(element => {
-    element.childNodes = element.childNodes.filter(element => element.nodeType == 1)
-    const day_info = element.childNodes.reduce((acc, current) => {
-        const info = current.childNodes[0].rawText;
-        switch (current.classNames[0]) {
-            case 'calendar-card__date':
-                acc.date = info
-                break;
-            case "calendar-card__entry-time":
-                acc.entry_time = info
-                break;
-            case "calendar-card__gym":
-                acc.gym = info
-                break;
-            case "calendar-card__class":
-                acc.time = info
-                break;
-            case "calendar-card__duration":
-                acc.duration = info
-                break;
-        }
-        return acc
-    }, {})
-    return day_info
-})
-console.log(activity)
+
+const get_activity =  () => {
+    const activity_html_string = fs.readFileSync(path.join(__dirname, "activity.html"), "utf8")
+    const root = htmlparser(activity_html_string)
+    const activity_html_array = root.querySelector(".calendar-column").childNodes.slice(4).filter(element => {
+        return element.nodeType == 1
+    })
+    const activity = activity_html_array.map(element => {
+        element.childNodes = element.childNodes.filter(element => element.nodeType == 1)
+        const day_info = element.childNodes.reduce((acc, current) => {
+            const info = current.childNodes[0].rawText;
+            switch (current.classNames[0]) {
+                case 'calendar-card__date':
+                    acc.date = info
+                    break;
+                case "calendar-card__entry-time":
+                    acc.entry_time = info
+                    break;
+                case "calendar-card__gym":
+                    acc.gym = info
+                    break;
+                case "calendar-card__class":
+                    acc.time = info
+                    break;
+                case "calendar-card__duration":
+                    acc.duration = info
+                    break;
+            }
+            return acc
+        }, {})
+        return day_info
+    })
+    return activity
+}
+module.exports = get_activity
