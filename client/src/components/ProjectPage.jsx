b797fe2a939b97124f1c6bbcf6383e23b09a5d20
Umair Darr <umerderr@gmail.com>
2020-02-19 18:54:52 +0000

    Add rendering of github readme on project page

--- client/src/components/ProjectPage.jsx
+++ client/src/components/ProjectPage.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
-
+import MarkdownRenderer from 'react-markdown/with-html'
 const ProjectPage = (props) => {
-    return (props.project_data ? <div>
-        {props.project_data.name}
+    return (props.project_data ? <div className="project-page">
+        <MarkdownRenderer source={props.project_data.readme}
+            escapeHtml={false} />
     </div> : <div></div>)
 }
 export default ProjectPage
+
